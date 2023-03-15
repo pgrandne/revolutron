e6174942557cf808110338fd0083b865f0844956
Perrin <perrin.grandne@protonmail.com>
2023-03-15 21:58:25 +0100

    change deck link

--- app/components/ModalInfo.tsx
+++ app/components/ModalInfo.tsx
@@ -21,7 +21,7 @@ export const ModalInfo = ({ tInfo, setModalInfo, deck }: {
                     }
                 </div>
                 {deck &&
-                    <p className="font-sans sm:block justify-center text-sm md:text-xl mb-4 font-bold ">{tInfo('deck')} ( <a className="underline" target="_blank" href="https://www.canva.com/design/DAFaoOaGU3A/Jj8ILlofzMJ5_gv8nlivKg/view?utm_content=DAFaoOaGU3A&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">{tInfo('online')}</a> / <a className="underline" target="_blank" href="https://drive.google.com/uc?id=1heBnCKRaljTbxGf0CcZR3-e65KASU53A&export=download">{tInfo('download')}</a> )</p>
+                    <p className="font-sans sm:block justify-center text-sm md:text-xl mb-4 font-bold ">{tInfo('deck')} ( <a className="underline" target="_blank" href="/">{tInfo('online')}</a> / <a className="underline" target="_blank" href="/">{tInfo('download')}</a> )</p>
                 }
                 {!deck &&
                     <p className="font-sans sm:block justify-center text-xs md:text-base mb-4 font-bold ">0x94b9420F65fB3ec966d96BB034b35AF86487D929</p>

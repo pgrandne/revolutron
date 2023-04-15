a0854e2af705151179ec0f4c159cf48dc64c1e5d
pgrandne <perrin.grandne@protonmail.com>
2023-04-15 17:39:50 +0200

    Add faucet and gas api

---
+++ app/api/faucet/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from 'next/server';
+import { ERC20 } from '@/utils/erc20'
+const TronWeb = require('tronweb');
+const fullNode = 'https://api.shasta.trongrid.io';
+const solidityNode = 'https://api.shasta.trongrid.io';
+const eventServer = 'https://api.shasta.trongrid.io';
+const privateKey = process.env.PRIVATE_KEY_SHASTA;
+
+export async function POST(request: Request) {
+    try {
+        const { address } = await request.json()
+        const tronWeb = new TronWeb(fullNode,solidityNode,eventServer,privateKey)
+        if (typeof process.env.PRIVATE_KEY !== "undefined" && typeof process.env.USDD_CONTRACT !== "undefined") {
+                let instance = await tronWeb.contract().at(process.env.USDD_CONTRACT)
+                let res = await instance.transfer(address,500).send({
+                    feeLimit:100_000_000,
+                    callValue:0,
+                    shouldPollResponse:true
+                });
+            }
+        else {
+                const error = new Error('No env variable')
+                error.name = 'env'
+                throw error
+        }
+        return NextResponse.json({ status: 200 })
+    } catch (_error) {
+        console.error(_error)
+        switch (_error.name) {
+            case 'env':
+                return NextResponse.json({
+                    message: _error.message
+                }, {
+                    status: 422,
+                })
+            case 'balance':
+                return NextResponse.json({
+                    message: _error.message
+                }, {
+                    status: 423,
+                })
+            default:
+                return NextResponse.json({
+                    message: "Unknown error"
+                }, {
+                    status: 500,
+                })
+        }
+    }
+}
---
+++ app/api/gas/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import { ERC20 } from '@/utils/erc20';
+const TronWeb = require('tronweb');
+const fullNode = 'https://api.shasta.trongrid.io';
+const solidityNode = 'https://api.shasta.trongrid.io';
+const eventServer = 'https://api.shasta.trongrid.io';
+const privateKey = process.env.PRIVATE_KEY_SHASTA;
+
+export async function POST(request: Request) {
+    try {
+        const { address } = await request.json()
+        const tronWeb = new TronWeb(fullNode,solidityNode,eventServer,privateKey)
+        if (typeof process.env.PRIVATE_KEY !== "undefined") {
+            tronWeb.trx.sendTransaction(address, 1000)
+            }
+        else {
+            const error = new Error('No env variable')
+            error.name = 'env'
+            throw error
+        }
+        return NextResponse.json({ status: 200, ok: true })
+    } catch (_error) {
+        console.error(_error)
+        switch (_error.name) {
+            case 'env':
+                return NextResponse.json({
+                    message: _error.message
+                }, {
+                    status: 422,
+                })
+            case 'balance':
+                return NextResponse.json({
+                    message: _error.message
+                }, {
+                    status: 423,
+                })
+            default:
+                return NextResponse.json({
+                    message: "Unknown error"
+                }, {
+                    status: 500,
+                })
+        }
+    }
+}

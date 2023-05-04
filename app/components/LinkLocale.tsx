70074d01726ff6428208043a70caa47f13019059
pgrandne <perrin.grandne@protonmail.com>
2023-05-04 16:14:24 +0200

    test openGraph 1

--- app/components/LinkLocale.tsx
+++ app/components/LinkLocale.tsx
@@ -1,24 +1,29 @@
-import { useLocale } from 'next-intl';
+import {useLocale} from 'next-intl';
 import NextLink from 'next/link';
-import { ComponentProps, forwardRef } from 'react';
-
+import {ComponentProps, forwardRef} from 'react';
+ 
 type Props = ComponentProps<typeof NextLink>;
-
-const LinkLocale = ({ href, ...rest }: Props, ref: Props['ref']) => {
-    const locale = useLocale();
-
-    function getLocalizedHref(originalHref: string) {
-        return originalHref.replace(/^\//, '/' + locale + '/');
-    }
-
-    const localizedHref =
-        typeof href === 'string'
-            ? getLocalizedHref(href)
-            : href.pathname != null
-                ? { ...href, pathname: getLocalizedHref(href.pathname) }
-                : href;
-
-    return <NextLink ref={ref} href={localizedHref} {...rest} />;
+ 
+function LinkLocale({href, ...rest}: Props, ref: Props['ref']) {
+  const locale = useLocale();
+ 
+  // Turn this off, to avoid updating the locale cookie for prefetch requests
+  const prefetch = false;
+ 
+  function getLocalizedHref(originalHref: string) {
+    return originalHref.replace(/^\//, '/' + locale + '/');
+  }
+ 
+  const localizedHref =
+    typeof href === 'string'
+      ? getLocalizedHref(href)
+      : href.pathname != null
+      ? {...href, pathname: getLocalizedHref(href.pathname)}
+      : href;
+ 
+  return (
+    <NextLink ref={ref} href={localizedHref} prefetch={prefetch} {...rest} />
+  );
 }
-
+ 
 export default forwardRef(LinkLocale);
